4730662edf9143999ed139730189dcadb208f8fa
Asher Schaffer <asher@squaretrade.com>
2015-12-29 10:14:29 -0800

    add table sorting with datatables

--- src/resources/views/list.blade.php
+++ src/resources/views/list.blade.php
@@ -3,8 +3,6 @@
 @section('title', trans_choice('notes::seat.note',2))
 @section('page_header', trans_choice('notes::seat.note',2))
 
-@inject('carbon', 'Carbon\Carbon')
-
 @section('character_content')
 
     <div class="panel panel-default">
@@ -19,16 +17,18 @@
         </div>
         <div class="panel-body">
 
-            <table class="table table-condensed table-hover table-responsive">
+            <table class="table datatable compact table-condensed table-hover table-responsive">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>{{ trans_choice('web::seat.title',1) }}</th>
+                        <th>{{ trans_choice('web::seat.detail', 2) }}</th>
+                        <th>{{ trans('notes::seat.updated_by') }}</th>
+                        <th>{{ trans('web::seat.updated') }}</th>
+                        <th></th>
+                    </tr>
+                </thead>
                 <tbody>
-                <tr>
-                    <th></th>
-                    <th>{{ trans_choice('web::seat.title',1) }}</th>
-                    <th>{{ trans_choice('web::seat.detail', 2) }}</th>
-                    <th>{{ trans('notes::seat.updated_by') }}</th>
-                    <th>{{ trans('web::seat.updated') }}</th>
-                    <th></th>
-                </tr>
 
                 @foreach($notes as $note)
 
@@ -42,7 +42,7 @@
                         <td>{{ str_limit($note->details, 50, '...') }}</td>
 
                         <td>{{ $note->name }}</td>
-                        <td>
+                        <td data-order="{{ $note->updated_at }}">
                           <span data-toggle="tooltip" title="" data-original-title="{{ $note->updated_at }}">
                             {{ human_diff($note->updated_at) }}
                           </span>
